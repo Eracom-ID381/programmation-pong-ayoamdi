20491968f64f34b8ff28de5ef7f948e649f46e40
Simon Pinkas <simon@code-think.com>
2020-06-03 15:43:32 +0200

    Make into OOP

--- js/sketch.js
+++ js/sketch.js
@@ -23,111 +23,137 @@ let ball = {
 //     height: 150
 // }
 
-let paddlesL;
-let paddlesR;
+let paddles = [];
+let paddleLeft;
+let paddleRight;
+
+let balls = [];
 
 
 
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    rectMode(CENTER);
     noStroke();
 
-    ball.x = width / 2;
-    ball.y = height / 2;
+    paddleRight = new Paddle(width - 30, 0, 20, 150, 'vertical'); //right
+    paddleLeft = new Paddle(30, 0, 20, 150, 'horizontal'); //left
 
-    // paddleRight.x = width - 30;
+    paddles.push(paddleRight);
+    paddles.push(paddleLeft);
 
-    paddlesR = new Paddles(width - 30, 0, 20, 150); //right
-    paddlesL = new Paddles(30, 0, 20, 150); //left
+    for (let i = 0; i < 1000; i += 1) { 
+        balls[i] = new Ball(width / 2, height / 2, 30, 10, 0);
+    }
 }
 
 function draw() {
     background(0);
     fill(255);
-    moveBall();
-    movePaddle()
-    paddlesR.bounceBall();
-    paddlesR.drawElements();
+    drawElements();
+    for (let i = 0; i < paddles.length; i += 1) { 
+        paddles[i].afficher();
+        paddles[i].bouger();
+    }
+   
+    for (let i = 0; i < balls.length; i += 1) {
+        balls[i].afficher();
+        balls[i].bouger();
+        balls[i].rebondir();
+        balls[i].score();
+    }
+}
 
-    paddlesL.bounceBall();
-    paddlesL.drawElements();
+function drawElements() {
+    ellipse(ball.x, ball.y, ball.radius);
+    textSize(100);
+    textAlign(RIGHT)
+    text(scoreLeft, width / 2 - 40, 100);
+    textAlign(LEFT)
 
+    text(scoreRight, width / 2 + 40, 100);
+
+    for (let y = 0; y < height; y = y + 30) {
+        rect(width / 2, y, 20, 20);
+    }
 }
 
 
-function moveBall() {
-    ball.x += ball.speedX;
-    ball.y += ball.speedY;
-}
-
-function resetBall() {
-    ball.x = width / 2;
-    ball.y = height / 2;
-    ball.speedX = -ball.speedX;
-    ball.speedY = random(-2, 2);
-
-}
-
-function movePaddle() {
-    paddlesR = mouseY;
-    paddlesL = mouseX;
-}
-
-class Paddles {
-    constructor(x, y, width, height) {
+class Paddle {
+    constructor(x, y, width, height, axis) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.axis = axis;
     }
-
-    drawElements() {
-        paddleR = mouseY;
-        paddleL = mouseX;
+    afficher() { 
         rect(this.x, this.y, this.width, this.height);
-        ellipse(ball.x, ball.y, ball.radius);
-        textSize(100);
-        textAlign(RIGHT)
-        text(scoreLeft, width / 2 - 40, 100);
-        textAlign(LEFT)
+    }
 
-        text(scoreRight, width / 2 + 40, 100);
-
-        for (let y = 0; y < height; y = y + 30) {
-            rect(width / 2, y, 20, 20);
+    bouger() { 
+        if (this.axis == 'vertical') {
+            this.y = mouseY;
+        } else if (this.axis == 'horizontal') { 
+            this.y = mouseX;
         }
     }
 
-    bounceBall() {
-        // Detection de collision Paddle Right
-        if (ball.x >= this.x - this.width * 2 &&
-            ball.y >= this.y - this.height / 2 &&
-            ball.y <= this.y + this.height / 2) {
-            ball.speedX = -ball.speedX;
-            ball.speedY = random(-5, 5);
+}
+
+function addBall() {
+    balls.push(new Ball(width/2, height/2, 30, random(-10, 10), 0));
+}
+
+class Ball {
+    constructor(_x, _y, _radius, _speedX, _speedY) {
+        this.x = _x;
+        this.y = _y;
+        this.radius = _radius;
+        this.speedX = _speedX;
+        this.speedY = _speedY;
+        this.distances = [];
+        this.enabled = true;
+        this.col = color(255,255,255);
+    }
+
+    afficher() {
+        fill(this.col);
+        ellipse(this.x, this.y, this.radius);
+    }
+    bouger() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+    rebondir() {
+        // Check for bounce against paddles
+        for (let i = 0; i < paddles.length; i += 1) {
+            if (this.x + this.radius / 2 > paddles[i].x &&
+                this.x - this.radius / 2 < paddles[i].x + paddles[i].width &&
+                this.y > paddles[i].y && this.y < paddles[i].y + paddles[i].height) {
+                this.speedX = -this.speedX;
+                this.speedY = random(-5, 5);
+            }
         }
 
-
-
-        // Detection collision "murs" haut et bas
-        if (ball.y <= ball.radius || ball.y >= height - ball.radius) {
-            ball.speedY = -ball.speedY;
+        // Check for bounce against edges
+        if (this.y > height - this.radius / 2 || this.y < 0 + this.radius / 2) {
+            this.speedY = -this.speedY;
         }
+    }
 
-        if (ball.x > width) {
-            resetBall('left');
-            scoreLeft += 1;
-        } else if (ball.x < 0) {
-            resetBall('right');
+    score() {
+        if (this.enabled && this.x < 0) {
             scoreRight += 1;
+            addBall();
+            this.enabled = false;
+        } else if (this.enabled && this.x > width) {
+            scoreLeft += 1;
+            addBall();
+            this.enabled = false;
         }
     }
-
-
-
-
 }
 
 function windowResized() {
